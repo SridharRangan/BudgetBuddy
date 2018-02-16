221239932bd5932924ad1d10523d497544a555cd
Willis Hand <willishand@gmail.com>
2018-02-16 11:08:08 -0500

    added android studio project

---
+++ BudgetBuddy/app/src/main/java/technology/thinkbench/budgetbuddy/CentralActivity.java
@@ -0,0 +1,23 @@
+package technology.thinkbench.budgetbuddy;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+public class CentralActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_central);
+        // Set up the login form.
+
+    }
+
+
+
+}
+
