37599f80c3aaf64314fbedf7f98c0a03d4c2b306
jasolorzano <altranperez@gmail.com>
2018-04-08 15:02:34 -0400

    added graph

--- BudgetBuddy/app/src/main/java/technology/thinkbench/budgetbuddy/MainFragment.java
+++ BudgetBuddy/app/src/main/java/technology/thinkbench/budgetbuddy/MainFragment.java
@@ -5,6 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 
 public class MainFragment extends android.support.v4.app.Fragment {
 
@@ -15,6 +19,15 @@ public class MainFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        GraphView graph = (GraphView) rootView.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 3)
+        });
+
+        graph.addSeries(series);
 
 
         return rootView;
